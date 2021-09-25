d840fef7601f4decbb4ff8239de8654fd170cad8
Филип Арсовски <arsovskidev@gmail.com>
2021-09-25 09:00:22 +0200

    Added favicon icon.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css" />
