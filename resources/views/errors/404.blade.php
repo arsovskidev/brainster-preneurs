6b9ff279ae7db0b5b435d46e3cab85901b6618b8
Филип Арсовски <arsovskidev@gmail.com>
2021-09-28 08:32:49 +0200

    Added custom error pages.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Whoops</title>
+
+    <link type="text/css" rel="stylesheet" href="{{ asset('css/errors.css') }}" />
+    <meta name="robots" content="noindex, follow" />
+</head>
+
+<body>
+    <div id="error">
+        <div class="error">
+            <div class="error-code">
+                <h1>404</h1>
+                <h2>Page not found</h2>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
